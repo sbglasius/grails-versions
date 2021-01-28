69a0487a573b78bb851d9c94731fe80be8e742ae
Søren Berg Glasius <soeren@glasius.dk>
2021-01-28 13:03:42 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,18 @@
+grails-versions
+---
+Adapted from Eric Helgeson who has archived his project.
+
+Handy repo/script to compare fresh `create-app` versions of Grails 3 so you can see what changed.
+
+Just compare across tags:
+* <https://github.com/sbglasius/grails-versions/compare/3.1.7...3.1.8>
+* <https://github.com/sbglasius/grails-versions/compare/3.2.0...3.3.12>
+* <https://github.com/sbglasius/grails-versions/compare/3.3.9...4.0.3>
+* <https://github.com/sbglasius/grails-versions/compare/4.0.0...4.0.6>
+* <https://github.com/sbglasius/grails-versions/compare/4.0.0...4.1.0.M2>
+
+From the git cli:
+
+```bash
+git diff 4.0.0..4.0.6
+```
---
+++ diff.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+git config user.name github-actions
+git config user.email github-actions@github.com
+
+export SDKMAN_DIR="$HOME/.sdkman" && source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install java 8.0.282-open
+
+for version in $(cat versions.txt)
+do
+    echo "Processing $version"
+    sdk install grails $version
+    sdk use grails $version
+    grails create-app versions
+    cd versions
+    ./gradlew dependencyManagement > ../dependencyManagement.txt
+    cd ..
+    git add versions
+    git commit -a -m $version
+    git push origin :refs/tags/$version
+    git tag --delete $version
+    git tag $version
+    rm -rf versions
+    sdk uninstall grails $version
+    df -h .
+done
+
+git commit -a -m "Cleanup"
+git push origin master -f
+git push origin --tags -f
+
+ls -l ~/.sdkman/archives/
